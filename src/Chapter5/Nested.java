1dc01c2a461c835c43d4da1b75e1d778d8d4ae8d
alzhan <aismagulov@gmail.com>
2019-10-31 09:51:19 +0100

    Chapter 5. Управляющие операторы. Nested

---
+++ src/Chapter5/Nested.java
@@ -0,0 +1,15 @@
+package Chapter5;
+
+//Циклы могут быть вложенными
+class Nested {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                System.out.print(".");
+                System.out.println();
+            }
+
+        }
+    }
+}
